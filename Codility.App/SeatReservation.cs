b4f62057194c0e754b2b991dfefe2c2a618b7b19
Taz Uddin <getleton@gmail.com>
2022-11-08 23:03:38 +0600

    Add helpful comments and more refactoring

--- Codility.App/SeatReservation.cs
+++ Codility.App/SeatReservation.cs
@@ -10,33 +10,32 @@ using System.Collections.Generic;
 class SeatReservation {
     public static int Solution(int N, string S) {
         
-        var validComb1 = "BCDE";
-        var validComb2 = "DEFG";
-        var validComb3 = "FGHJ";
+        var possibility1 = "BCDE";
+        var possibility2 = "DEFG";
+        var possibility3 = "FGHJ";
 
+        // First all possible combinations are gathered, then start cancelling out as the
+        // reserved seat number is read from the string S
         var possibilities = new Dictionary<int, List<string>>();
         for (int i = 1; i <= N; i++) {
-            possibilities.Add(i, new List<string>(){validComb1, validComb2, validComb3});
+            possibilities.Add(i, new List<string>(){possibility1, possibility2, possibility3});
         }
 
         if (S != null && S.Length > 0) {
             var reservations =  S.Split(' ');        
             reservations.ToList().ForEach(seat => {  
-                var rowNum = seat.Length == 2 ? int.Parse(seat.Substring(0, 1)) : int.Parse(seat.Substring(0, 2));
-                if (rowNum == 20) {
-                    Console.WriteLine("Here");
-                }
+                var rowNum = seat.Length == 2 ? int.Parse(seat.Substring(0, 1)) : int.Parse(seat.Substring(0, 2));                
                 if (possibilities.TryGetValue(rowNum, out var row)) {
-                    var col = seat[seat.Length - 1].ToString();
-                    var p = row.Where(comb => !comb.Contains(col)).ToList();
-                    possibilities[rowNum] = p;
+                    var colNum = seat[seat.Length - 1].ToString();
+                    var filtered = row.Where(p => !p.Contains(colNum)).ToList();
+                    possibilities[rowNum] = filtered;
                 }
             });
         }
         
-        var values = possibilities.ToList()
-        .Select(pair => pair.Value)
-        .Select(list => {
+        // Now that we have canceled out all the reserved seats from potentially reservable list of seats, we need to 
+        // filter out any remaining invalid seat combination.
+        var values = possibilities.ToList().Select(pair => pair.Value).Select(list => {
             var d = new HashSet<char>();
             list.ForEach(str => {
                 var ch  = str.ToCharArray();
